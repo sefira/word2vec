34cf381d81b2e374fbdb0ff1b5cd9c668ea9749c
Xingyuan Bu <sefira32@gmail.com>
2017-06-15 12:19:14 +0000

    update test_word2vec.py

--- src/test_word2vec.py
+++ src/test_word2vec.py
@@ -5,10 +5,11 @@ import multiprocessing
 from gensim.models import Word2Vec
 
 #################### config ###################
-modelfile = "../model/review_douban_movie.tsv.removewordsize300window5sg1min_count100negative10iter25.model"
+modelfile = "../model/size300window5sg1min_count100negative10iter50.model"
 questionfile = "../data/questions-words-Zh.txt"
 ############### end of config #################
 
+logger = logging.getLogger()
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
 logging.root.setLevel(level=logging.INFO)
 logger.info("running test word2vec for model: %s" % modelfile)
